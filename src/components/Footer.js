31fe1e76a356c81a02a847de1733d186ec99c195
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-10-17 18:43:02 +0900

    ♻️ refactor: clean up and componentize style files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-
+import styles from "@/components/Footer.module.css";
+ 
 export function Footer() {
   return (
     <footer className={styles.footer}>
