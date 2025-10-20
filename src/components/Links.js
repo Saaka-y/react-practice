a17ee24b1d8203d680029190df0208b524eabaa9
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-10-20 16:06:16 +0900

    ♻️ refactor: clean up Links components

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,32 +1,45 @@
 import Image from "next/image";
 import styles from "@/components/Links.module.css";
 
+// データと処理は分離する
+const ITEMS = [
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy now",
+    isPrimary: true,
+    imgSrc: "/vercel.svg",
+    imgAlt: "Vercel logomark",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Read our docs",
+    isPrimary: false,
+  },
+]
+
+
 export function Links() {
   return (
     <div className={styles.ctas}>
-      <a
-        className={styles.primary}
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          className={styles.logo}
-          src="/vercel.svg"
-          alt="Vercel logomark"
-          width={20}
-          height={20}
-        />
-        Deploy now
-      </a>
-      <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.secondary}
-      >
-        Read our docs
-      </a>
+      {ITEMS.map(item => {
+        return (
+          <a key={item.href} className={styles.primary}
+            href={item.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.imgSrc && (<Image
+              className={styles.logo}
+              src={item.imgSrc}
+              alt={item.imgAlt}
+              width={20}
+              height={20}
+            />
+            )}
+            {item.title}  
+          </a>
+        )
+      })}
     </div>
   );
 }
