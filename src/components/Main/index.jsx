bf21a1fb67fa7a4f66b1677010c02bf0f6f5a135
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-10-23 17:33:12 +0900

    ✨ install counter button and count up with useState

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,12 +6,10 @@ import { useEffect } from "react";
 export function Main(props) {
   // return前の処理はマウント時に実行される
   useEffect(() => {
-    console.log("maunted");
     document.body.style.backgroundColor = "cadetblue";
 
     // return以降はアンマウント時の処理
     return () => {
-      console.log("unmaunted");
       document.body.style.backgroundColor = "";
     }
   }, []); // 一旦第二引数は無視！
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useCallback, useEffect } from "react";
+import { useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +18,15 @@ const geistMono = Geist_Mono({
 
 // コンポーネントは中にある関数もまとめて全て再レンダリングされる
 export default function Home() {
-  const handleClick = useCallback((e) => { // useCallbackは関数の再レンダリングを防ぐ
-    console.log(e.target);
-    e.preventDefault();
-  }, []); 
+  const [count, setCount] = useState(1);
+  // これは配列の分割代入
+  // const array = useState(1);, const foo = array[0];, const setFoo = array[1];と同じ
+
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+    setCount(count => count + 1); //アロー関数で最新のstateを受け取る
+    // setCount(foo + 1); でも動くが、複数回呼ばれた場合に古いstateを参照してしまう（1 + 1）を一生やっているのと同じ挙動
+  };
 
   return (
     <>
@@ -29,12 +34,12 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a
-        href="/about"
-        onClick={handleClick}
-      >
-        Test
-      </a>
+      <h1 style={{ textAlign: "center", margin: "1rem" }}>{count}</h1>
+      <button
+        style={{ display: "block", margin: "1rem auto", padding: "10px", border: "none", backgroundColor: "white"}}
+        onClick={handleClick}>
+        Click Me
+      </button>
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
